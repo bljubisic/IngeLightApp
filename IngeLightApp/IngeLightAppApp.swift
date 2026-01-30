432dc51d57778dc94e2ad004c566ca02dc8fd4fb
Ljubisic,Bratislav (BI X) BIX-DE-I <bratislav.ljubisic@boehringer-ingelheim.com>
2026-01-30 08:36:50 +0100

    Initial Commit

---
+++ IngeLightApp/IngeLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngeLightAppApp.swift
+//  IngeLightApp
+//
+//  Created by Bratislav Ljubisic on 30.01.26.
+//
+
+import SwiftUI
+
+@main
+struct IngeLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
